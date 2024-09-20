9fc0beaaa9ce48b273402be8456126aa677ea350
Wonjong Kim <bonk159@gmail.com>
2024-09-20 13:13:12 +0900

    project init

---
+++ backend/cap-backend/src/main/java/capstone/capbackend/CapBackendApplication.java
@@ -0,0 +1,13 @@
+package capstone.capbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapBackendApplication.class, args);
+    }
+
+}
---
+++ backend/cap-backend/src/test/java/capstone/capbackend/CapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.capbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
